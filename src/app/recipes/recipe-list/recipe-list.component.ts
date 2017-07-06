2c669dc3ddf58d59b8288c7cfa5479722110dfd2
fernando de la via <fernando.delavia@xtime.com>
2017-07-06 14:42:18 -0400

    Using a service for cross-component  comunication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { RecipeService } from '../recipe.service';
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,17 +8,12 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 export class RecipeService{ 
+    recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
             new Recipe('A test recipe','This is simply a test','https://www.bbcgoodfood.com/sites/default/files/styles/carousel_medium/public/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
             new Recipe('A test recipe 2','This is simply a test','https://www.bbcgoodfood.com/sites/default/files/styles/carousel_medium/public/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
